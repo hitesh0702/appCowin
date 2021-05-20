791d4a2e2bec36edd9d654309260e3730f7ebdcb
Hitesh <73782583+hitesh0702@users.noreply.github.com>
2021-05-20 07:53:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,9 @@ min_age_limit = st.radio('Min age limit', [18, 45])
 option = st.multiselect('District', avail_districts, "Ahmedabad Corporation")
 
 district_ids = [mapper[val] for val in option]
-
+try:
+    df = get_availability(district_ids, min_age_limit)
+    st.table(df)
+except:
+    st.error('Unable to fetch data. Try after a few minutes')
 
